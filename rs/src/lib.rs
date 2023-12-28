3eb8db3aa840724b07378f9c1b8ef9c666d205a6
Aaron Christiansen <aaronc20000@gmail.com>
2023-12-28 15:50:31 +0000

    Bridge more functions

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 extern "C" {
+    fn eadk_bridge__keyboard_scan() -> u64;
     fn eadk_bridge__display_push_rect_uniform(x: u16, y: u16, width: u16, height: u16, color: u16);
     fn eadk_bridge__display_draw_string(str: *const u8, x: u16, y: u16, large_font: u8, text_color: u16, bg_color: u16);
 }
@@ -10,6 +11,16 @@ pub extern "C" fn rs_main() {
     unsafe {
         eadk_bridge__display_push_rect_uniform(10, 10, 10, 10, 0xe426);
         eadk_bridge__display_draw_string(b"Hello from Rust!\0".as_ptr(), 50, 50, 0, 0, 0xFFFF);
+        
+        loop {
+            let colour =
+                if (eadk_bridge__keyboard_scan() & 0b1) > 0 {
+                    0
+                } else {
+                    0xFFFF
+                };
+            eadk_bridge__display_push_rect_uniform(50, 100, 20, 20, colour);
+        }
     }
 }
 
--- src/eadk_bridge.c
+++ src/eadk_bridge.c
@@ -1,16 +1,55 @@
 // Adapts EADK functions with "simpler" type signatures, allowing them to be called from Rust
 // without problems.
 //
+// For consistency, even functions which could probably be called from Rust without problems are
+// bridged here.
+//
 // Targets the `eadk.h` distributed with `nwlink` 0.0.17.
 
 #include <eadk.h>
 #include <stdint.h>
 
-void eadk_bridge__display_push_rect_uniform(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t color) {
-  eadk_display_push_rect_uniform((eadk_rect_t){ x, y, width, height }, (eadk_color_t){color});
+// Keyboard and Events
+
+uint64_t eadk_bridge__keyboard_scan() {
+    return eadk_keyboard_scan();
 }
 
-void eadk_bridge__display_draw_string(char *str, uint16_t x, uint16_t y, uint8_t large_font, uint16_t text_color, uint16_t bg_color) {
-  eadk_display_draw_string(str, (eadk_point_t){x, y}, large_font, text_color, bg_color);
+// Display
+
+void eadk_bridge__display_push_rect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const uint16_t* pixels) {
+    eadk_display_push_rect((eadk_rect_t){ x, y, width, height }, pixels);
 }
+
+void eadk_bridge__display_push_rect_uniform(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t color) {
+    eadk_display_push_rect_uniform((eadk_rect_t){ x, y, width, height }, (eadk_color_t){color});
+}
+
+void eadk_bridge__display_pull_rect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, eadk_color_t * pixels) {
+    eadk_display_push_rect((eadk_rect_t){ x, y, width, height }, pixels);
+}
+
+bool eadk_bridge__display_wait_for_vblank() {
+    return eadk_display_wait_for_vblank();
+}
+
+void eadk_bridge__display_draw_string(const char *str, uint16_t x, uint16_t y, uint8_t large_font, uint16_t text_color, uint16_t bg_color) {
+    eadk_display_draw_string(str, (eadk_point_t){x, y}, large_font, text_color, bg_color);
+}
+
+// Timing
+
+void eadk_bridge__timing_usleep(uint32_t us) {
+    eadk_timing_usleep(us);
+}
+
+void eadk_bridge__timing_msleep(uint32_t ms) {
+    eadk_timing_msleep(ms);
+}
+
+uint64_t eadk_bridge__timing_millis() {
+    return eadk_timing_millis();
+}
+
+
 
