aef36edbf2edde602cb5f636663749a09fded7dc
Aaron Christiansen <aaronc20000@gmail.com>
2023-12-28 17:30:42 +0000

    Wrap more display stuff

--- rs/src/eadk.rs
+++ rs/src/eadk.rs
@@ -35,6 +35,16 @@ pub mod display {
         pub width: u16,
         pub height: u16,
     }
+    
+    /// A collection of sized pixel data which can be drawn to the screen as an image.
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub struct Bitmap<'d> {
+        pub width: u16,
+        pub height: u16,
+
+        // Expected to have size (width * height)
+        pub data: &'d [Color],
+    }
 
     /// A point on the display.
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -45,7 +55,15 @@ pub mod display {
 
     /// An RGB565 color.
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    #[repr(C)]
     pub struct Color(pub u16);
+    impl Color {
+        pub const WHITE: Color = Color(0xFFFF);
+        pub const BLACK: Color = Color(0);
+        pub const RED:   Color = Color(0xF800);
+        pub const GREEN: Color = Color(0x07E0);
+        pub const BLUE:  Color = Color(0x001F);
+    }
 
     /// The fonts available for drawing text.
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -67,11 +85,29 @@ pub mod display {
         }
     }
 
+    /// Draws a [Bitmap] to a given [Point] on the display.
+    pub fn draw_bitmap(pt: Point, bitmap: Bitmap) {
+        // TODO: validate dimensions
+        unsafe {
+            super::eadk_bridge__display_push_rect(pt.x, pt.y, bitmap.width, bitmap.height, bitmap.data.as_ptr() as *const u16)
+        }
+    }
+
     /// Writes a null-terminated sequence of ASCII bytes to the display.
     /// 
     /// It is the caller's responsibility to ensure that the given slice contains a NUL (0).
     /// Undefined Behaviour will occur if it doesn't.
     pub unsafe fn write_string_null_terminated(str: &[u8], pt: Point, font: Font, text_color: Color, bg_color: Color) {
         super::eadk_bridge__display_draw_string(str.as_ptr(), pt.x, pt.y, font.to_large_font_arg(), text_color.0, bg_color.0)
+    }
+
+    /// Waits for display VBLANK.
+    /// 
+    /// Honestly, I don't know what this does, but I'll bridge it anyway in case it's important for
+    /// something.
+    pub fn wait_for_vblank() -> bool {
+        unsafe {
+            super::eadk_bridge__display_wait_for_vblank() > 0
+        }
     }
 }
--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,30 +1,47 @@
 #![no_std]
 
-use eadk::{display::{self, Rect, Color, Font, Point}, input};
+use eadk::{display::{self, Rect, Color, Font, Point, Bitmap}, input};
 
 pub mod eadk;
 
 #[no_mangle]
 pub extern "C" fn rs_main() {
     unsafe {
+        // Rectangle test
         display::fill(
             Rect { x: 10, y: 10, width: 10, height: 10 },
             Color(0xe426),
         );
+
+        // Text test
         display::write_string_null_terminated(
             b"Hello from Rust!\0",
             Point { x: 50, y: 50 },
             Font::Large,
-            Color(0),
-            Color(0xFFFF)
+            Color::BLACK,
+            Color::WHITE,
         );
-        
+
+        // Bitmap writing test
+        let mut bitmap_write_data = [Color::RED; 4 * 4];
+        bitmap_write_data[0] = Color::BLUE;
+        bitmap_write_data[(4 * 4) - 1] = Color::GREEN;
+        display::draw_bitmap(
+            Point { x: 2, y: 2 },
+            Bitmap {
+                width: 4,
+                height: 4,
+                data: &bitmap_write_data,
+            },
+        );
+
+        // Input test
         loop {
             let colour =
                 if (input::keyboard_scan() & 0b1) > 0 {
-                    Color(0)
+                    Color::BLACK
                 } else {
-                    Color(0xFFFF)
+                    Color::WHITE
                 };
             display::fill(
                 Rect { x: 50, y: 100, width: 20, height: 20 },
